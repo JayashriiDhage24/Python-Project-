b00e80bd775dfbc1f35811472fd4ef805c356e27
Jayashri Dhage <156888528+JayashriiDhage24@users.noreply.github.com>
2025-03-04 22:47:59 +0530

    Add files via upload

---
+++ takepaymentonline.py
@@ -0,0 +1,26 @@
+import qrcode
+
+#takink up id as a input 
+upi_id =input("Enter your UPI ID =")
+
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+#save qr code to image file (opetional)
+
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+#Display the qr codes (You may need to install PIL/Pillow Library)
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
+
+
